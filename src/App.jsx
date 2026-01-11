8cf16099a61e0968827f04a376455a5a20fcaf08
Favour Egwele <favoregwele@gmail.com>
2026-01-11 23:04:15 +0100

    Setting up router

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
 import Header from "./Header.jsx";
 import Body from "./Body.jsx";
 import About from "./About.jsx"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
 
   return (
-    <main className='test'>
-      <Header/>
-      <Body />
-      <About/>
-    </main>
+    <BrowserRouter>
+      <main className='test'>
+
+        <Header/>
+        
+        <Routes>
+          <Route path="/" element={<Body />}/>
+          <Route path="/about" element={<About />}/>
+        </Routes>
+      
+      </main>
+    </BrowserRouter>
   )
 }
 
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,11 +1,20 @@
+import { Link } from "react-router-dom";
+
 function Header() {
     return ( 
         <header className="sticky">
             <nav>
                 <img src="/images/Logo-InternetFae.png" alt="" />
                 <div className="nav-box">
-                    <a href=""><button className="navigating">Portfolio</button></a>
-                    <a href=""><button className="navigating">About me</button></a>
+                    
+                    <Link to="/">
+                        <button className="navigating">Portfolio</button>
+                    </Link>
+
+                    <Link to="/about">
+                        <button className="navigating">About me</button>
+                    </Link>
+
                 </div>
             </nav>
         </header>
