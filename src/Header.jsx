dcdee6a315252c6e9c2ccf8904fbdbe76abcc9ce
Favour Egwele <favoregwele@gmail.com>
2026-01-12 13:38:04 +0100

    Converting to tailwind + media query: header

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,15 +1,36 @@
+import { useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 
 function Header() {
 
+    const [optSm, setOptSm] = useState(false);
     const location = useLocation();
-    console.log('loca:', location);
 
     return ( 
         <header className="sticky">
             <nav>
                 <img src="/images/Logo-InternetFae.png" alt="" />
-                <div className="nav-box">
+
+                <div className="nav-box-sm lg:hidden">
+                    <button className="bi bi-list nav-icon" onClick={() => setOptSm(prev => !prev)}></button>
+                    {optSm && (
+    
+                        <div className="navigating-sm">
+
+                            <Link to="/">
+                                <button className={`navigation ${location.pathname === "/" ? "selected-sm" : ""}`} onClick={() => setOptSm(prev => !prev)}>Portfolio</button>
+                            </Link>
+
+                            <Link to="/about">
+                                <button className={`navigation ${location.pathname === "/about" ? "selected-sm" : ""}`} onClick={() => setOptSm(prev => !prev)}>About me</button>
+                            </Link>
+
+                        </div>
+
+                    )}
+                </div>                
+
+                <div className="nav-box hidden lg:flex">
                     
                     <Link to="/">
                         <button className={`navigating ${location.pathname === "/" ? "boxy" : ""}`}>Portfolio</button>
