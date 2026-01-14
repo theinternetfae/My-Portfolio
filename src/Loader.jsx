6849484f8a8124bb349592ff823c35d4d03cfcab
Favour Egwele <favoregwele@gmail.com>
2026-01-14 21:50:14 +0100

    Finished loader screen animation

--- src/Loader.jsx
+++ src/Loader.jsx
@@ -1,9 +1,22 @@
 function Loader() {
     return ( 
         <section className="loader">
+            
+            <div className="load">
+                <span style={{"--i": 1}} className="--i"></span>
+                <span style={{"--i": 2}} className="--i:2"></span>
+                <span style={{"--i": 3}} className="--i:3"></span>
+                <span style={{"--i": 4}} className="--i:4"></span>
+                <span style={{"--i": 5}} className="--i:4"></span>
+                <span style={{"--i": 6}} className="--i:4"></span>
+                <span style={{"--i": 7}} className="--i:4"></span>
+                <span style={{"--i": 8}} className="--i:4"></span>
+            </div>
+            
             <p>
                 Loading...
             </p>
+        
         </section>
     );
 }
