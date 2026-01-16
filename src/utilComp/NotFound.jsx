aacfc4b047916eefabf1cdc39d5748bad263fe21
Favour Egwele <favoregwele@gmail.com>
2026-01-16 19:44:18 +0100

    Donegit add .!

--- src/utilComp/NotFound.jsx
+++ src/utilComp/NotFound.jsx
@@ -1,7 +1,7 @@
 function NotFound({ found }) {
     return ( 
             <section className="blur-error">
-                <img src="/images/Error-not-Found.png" alt="not-found" />
+                <img src="./images/Error-not-Found.png" alt="not-found" />
                 <p>It seems that doesn't exist in my portfolio. Maybe you made a typo?</p>
                 <button onClick={() => found(prev => !prev)}>Go back</button>
             </section>
