446e5a44dc62fb39fb711e7fc94a2549543a8af8
Favour Egwele <favoregwele@gmail.com>
2026-04-20 00:45:13 +0200

    Designed landing page

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -1,8 +1,26 @@
 function Landing() {
     return ( 
         <section className="landing-page">
-            <p>TheInternetFae</p>
             
+            <div className="name-cont">
+                <p>Hi, I'm <span>TheInternetFae</span></p>
+            </div>
+            
+            <div className="title-cont">
+                FRONT-END DEVELOPER
+            </div>
+            
+            <div className="main">
+                <div className="img-cont">
+                    <img src="./images/Me.jpg" alt="" />
+                </div>
+                <h1>MY PORTFOLIO</h1>
+                <div className="land-directions">
+                    <button className="about">About</button>
+                    <button className="projects">Projects</button>
+                </div>
+            </div>
+
         </section>
     );
 }
