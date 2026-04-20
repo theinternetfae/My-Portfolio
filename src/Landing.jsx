11d65d7f9c9b520fccfab897cb4c12e7a8a67c39
Favour Egwele <favoregwele@gmail.com>
2026-04-20 02:37:49 +0200

    Media query for landing page

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -1,15 +1,17 @@
 function Landing() {
     return ( 
         <section className="landing-page">
-            
-            <div className="name-cont">
-                <p>Hi, I'm <span>TheInternetFae</span></p>
+
+            <div className="intro">
+                <div className="name-cont">
+                    <p>Hi, I'm <span>TheInternetFae</span></p>
+                </div>
+                
+                <div className="title-cont">
+                    FRONT-END DEVELOPER
+                </div>
             </div>
-            
-            <div className="title-cont">
-                FRONT-END DEVELOPER
-            </div>
-            
+
             <div className="main">
                 <div className="img-cont">
                     <img src="./images/Me.jpg" alt="" />
